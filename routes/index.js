f5c22915f544f741fab5dec130f10200a1f20ca6
salierri <g.gergely314@gmail.com>
2017-02-05 21:45:05 +0100

    Added socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var http = require('http').createServer();
+var io = require('socket.io')(http);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile('index.html');
 });
 
 module.exports = router;
