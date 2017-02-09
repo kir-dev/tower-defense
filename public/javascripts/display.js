ac4a8b2ba34a8c8de227c24ab42c12bf3db76c6d
salierri <g.gergely314@gmail.com>
2017-02-09 20:53:14 +0100

    Laser shots on frontend

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -4,6 +4,7 @@ var Display = (function() {
     var enemies = [];
     var towers = [];
     var imageCache = {};
+    var shots = [];
 
     module.setEnemies = function (message) {
         enemies = message;
@@ -11,6 +12,11 @@ var Display = (function() {
 
     module.setTowers = function (message) {
         towers = message;
+    }
+
+    module.shoot = function (message) {
+        getTowerAt(message.tower.x, message.tower.y).target = {x: message.enemy.x, y: message.enemy.y};
+        shots.push(message);
     }
 
     module.draw = function () {
@@ -22,12 +28,41 @@ var Display = (function() {
             context.drawImage(imageCache.greenplane, translate(enemy.x), translate(enemy.y));
         });
         towers.forEach(function (tower) {
-            context.drawImage(imageCache.doublerocket, translate(tower.x), translate(tower.y));
+            if(tower.target) {
+                tower.angle = Math.atan2(tower.y - tower.target.y, tower.x - tower.target.x) - Math.PI / 2;
+            }
+            rotateAndPaintImage(context, imageCache.doublerocket, tower.angle, translate(tower.x), translate(tower.y), 32, 32);
         });
+        shots.forEach(function (shot) {
+            context.beginPath();
+            context.moveTo(translate(shot.tower.x), translate(shot.tower.y));
+            context.lineTo(translate(shot.enemy.x), translate(shot.enemy.y));
+            context.strokeStyle = '#ff0000';
+            context.stroke();
+        });
+        shots = [];
     }
 
     function translate (coord) {
         return (coord - 0.5) * 50 - 32;
+    }
+
+    function getTowerAt(x, y) {
+        var ret = null;
+        towers.forEach(function (tower) {
+            if(tower.x === x && tower.y === y) {
+                ret = tower;
+            }
+        });
+        return ret;
+    }
+
+    function rotateAndPaintImage (context, image, angleInRad , positionX, positionY, axisX, axisY) {
+        context.translate(positionX, positionY);
+        context.rotate(angleInRad);
+        context.drawImage(image, -axisX, -axisY);
+        context.rotate(-angleInRad);
+        context.translate(-positionX, -positionY);
     }
 
     function cacheImage(name, tileId) {
@@ -39,7 +74,7 @@ var Display = (function() {
     }
 
     cacheImage('greenplane', 270);
-    cacheImage('doublerocket', 205);
+    cacheImage('doublerocket', 206);
 
     return module;
 })();
