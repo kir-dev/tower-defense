b3637a310294d8de5c4b766787b8be66dfd0d0d3
kpillis <pillis.krisztian@gmail.com>
2017-02-09 23:57:48 +0100

    Orders scoreboard by point

--- public/javascripts/scoreboard.js
+++ public/javascripts/scoreboard.js
@@ -1,8 +1,17 @@
 var Scoreboard = (function () {
     var module = {};
 
+    function compare(a,b) {
+      if (a.point< b.point)
+        return 1;
+      if (a.point > b.point)
+        return -1;
+      return 0;
+    }
+
     module.update = function(message) {
         var scoreboardText = "";
+        message.sort(compare);
         message.forEach(function(user) {
             scoreboardText = scoreboardText + "<tr><td><b>"
             + user.username + "</b></td><td>"
